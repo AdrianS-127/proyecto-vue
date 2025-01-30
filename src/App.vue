63d7d85d28c226f7c95559a21a14cf2aa16597ca
Adrián Santos <a23311062@uthermosillo.edu.mx>
2025-01-29 18:44:19 -0700

    Se agrego la lista de tareas

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,11 @@
 <template>
   <Contador />
+  <ListDeTareas />
 </template>
 
 <script setup>
 import Contador from './components/Contador.vue';
+import ListDeTareas from './components/ListDeTareas.vue';
 </script>
 
 <style scoped>
