6ff8a885dea54ecec928f0f57c3192d9ff3a7f73
Adrián Santos <a23311062@uthermosillo.edu.mx>
2025-01-30 17:52:02 -0700

    Crea Store de pinia para almacenar los datos de registro

--- src/components/SideBar.vue
+++ src/components/SideBar.vue
@@ -1,6 +1,8 @@
 <template>
 <div class="sidebar">
-    <H2>Barra Lateral</H2>
+    <h2>Bienvenido</h2>
+    <h3>Nombre: {{ nombre }}</h3>
+    <h3>Email: {{ email }}</h3>
     <ul>
         <li><router-link to="/Contador">Contador</router-link></li>
         <li><router-link to="/ListDeTareas">Lista de Tareas</router-link></li>
@@ -11,6 +13,19 @@
 </template>
 
 <script setup>
+import { useRegistrarStore } from '@/modules/registro/stores/registrarStore';
+import { ref, watch } from 'vue';
+const registrarStore = useRegistrarStore();
+const nombre = ref(registrarStore.nombre);
+const email = ref(registrarStore.email);
+watch(
+  () => [registrarStore.nombre, registrarStore.email], // Propiedades observadas
+([newNombre, newEmail]) => {
+    nombre.value = newNombre;
+    email.value = newEmail;
+}
+);
+
 
 </script>
 
--- src/modules/registro/views/Registrar.vue
+++ src/modules/registro/views/Registrar.vue
@@ -4,34 +4,43 @@
         <Form :validation-schema="schema" @submit="onSubmit">
             <div class="form">
                 <label for="nombre">Nombre:</label>
-                <Field type="text" name="nombre" id="nombre" placeholder="Ingrese su nomnbre"/>
-                <ErrorMessage name="nombre" />
+                <Field v-model="nombre" type="text" name="nombre" id="nombre" placeholder="Ingrese su nombre"/>
+                <ErrorMessage name="nombre"></ErrorMessage>
             </div>
             <div class="form">
-                <label for="email">Email:</label>
-                <Field type="email" name="email" id="email" placeholder="Ingrese su email"/>
+                <label for="correo">Email:</label>
+                <Field v-model="email" type="email" name="email" id="correo" placeholder="Ingrese su email"/>
                 <ErrorMessage name="email"></ErrorMessage>
             </div>
             <div class="form">
                 <button type="submit">Registrar</button>
             </div>
-            
         </Form>
     </div>
 </template>
 
 <script setup>
-    import {Form, Field, ErrorMessage} from 'vee-validate';
-    import {schema} from '../schemas/validationSchema';
+import {Form, Field, ErrorMessage} from 'vee-validate';
+import {schema} from '../schemas/validationSchema';
+import {useRegistrarStore} from '../stores/registrarStore';
+import {ref} from 'vue';
 
-    const onSubmit = (values) => {
-        console.log('Se ha enviado el formulario');
-    }
+const nombre = ref('');
+const email = ref('');
+const registrarStore = useRegistrarStore();
 
+const onSubmit = (values) => {
+    registrarStore.guardarRegistro(nombre.value, email.value);
+    console.log('Se ha enviado el formulario con los datos:', values);
+};
 </script>
 
 <style scoped> 
-.form{
+.form {
     margin-bottom: 10px;
+}
+.error-message {
+    color: red;
+    font-size: 0.9em;
 }
 </style>
